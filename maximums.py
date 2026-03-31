db732b462d715eed3da9089fcd1b2d9fca0ba57d
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-03-31 10:55:32 -0300

    Implement max_of_two and max_of_three functions

--- maximums.py
+++ maximums.py
@@ -1,10 +1,16 @@
-# Replace the "ANSWER HERE" for your answer
-
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
+    if x > y:
+        maximo = x
+    else:
+        maximo = y
+    return maximo
 
 
 def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    if x > y and x > z:
+        maximo = x
+    elif y > x and y > z:
+        maximo = y
+    else:
+        maximo = z
+    return maximo
